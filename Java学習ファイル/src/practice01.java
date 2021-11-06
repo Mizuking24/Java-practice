c49c8291212c5e9c1f886cc58d55d484a4b8e2ca
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-11-06 18:57:53 +0900

    [Finish]Java入門-practise01

---
+++ Java学習ファイル/src/practice01.java
@@ -0,0 +1,20 @@
+
+public class practice01 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+
+		boolean answer = true;
+		char character = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String msg = "ミナトの攻撃！的に15ポイントのダメージを与えた";
+		System.out.println(answer);
+		System.out.println(character);
+		System.out.println(pi);
+		System.out.println(number);
+		System.out.println(msg);
+	}
+}
