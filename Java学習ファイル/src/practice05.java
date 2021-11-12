75227ac07c9bba29567b5df799779151dffc6007
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-11-12 20:36:20 +0900

    [Finish]Java入門-practice05

---
+++ Java学習ファイル/src/practice05.java
@@ -0,0 +1,60 @@
+
+public class practice05 {
+//	練習5-1
+//	public static void main(String[] args) {
+//		introduceOneself();
+//	}
+//	public static void introduceOneself() {
+//		String name = "吉田";
+//		int age = 22;
+//		double size = 170.0;
+//		char zodiac = '寅';
+//		System.out.println("私の名前は" + name + "です。");
+//		System.out.println("年齢は" + age + "です。");
+//		System.out.println("身長は" + size + "です。");
+//		System.out.println("干支は" + zodiac + "です。");
+//	}
+	
+//	練習5-2
+//	public static void main(String[] args) {
+//		String title = "お誘い";
+//		String address = "mimimi@iclo.com";
+//		String text = "今度一緒にご飯にいきませんか？";
+//		email(title, address, text);
+//	}
+//	public static void email(String title, String address, String text) {
+//		System.out.println(address + "に、以下のメールを送信しました。");
+//		System.out.println("件名: " + title);
+//		System.out.println("本文: " + text);
+//	}
+//	
+/// 練習5-3(練習5-2のemailメソッドを使う)
+//	public static void email(String address, String text) {
+//		System.out.println(address + "に、以下のメールを送信しました。");
+//		System.out.println("件名: 無題");
+//		System.out.println("本文: " + text);
+//	}
+//	public static void main(String[] args) {
+//		String title = "お誘い";
+//		String address = "mimimi@iclo.com";
+//		String text = "今度一緒にご飯にいきませんか？";
+//		email(title, address, text);
+//		email(address, text);
+//	}
+	
+//	練習5-4
+//	public static double calcTriangleArea(double bottom, double height) {
+//		double ans = bottom * height / 2;
+//		return ans;
+//	}
+//	public static double calcCircleArea(double radius) {
+//		double ans = radius * radius * 3.14;
+//		return ans;
+//	}
+//	public static void main(String[] args) {
+//		double triangleArea = calcTriangleArea(10.0, 5.0);
+//		System.out.println("三角形の面積 = " + triangleArea + "平方cm");
+//		double circleArea = calcCircleArea(5.0);
+//		System.out.println("円の面積 = " + circleArea + "平方cm");
+//	}
+}
