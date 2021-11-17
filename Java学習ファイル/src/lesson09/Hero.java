1bc274d6f3f977b919bd4878c40519c959a0b3e5
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-11-17 17:48:00 +0900

    [Start]Java入門-10

---
+++ Java学習ファイル/src/lesson09/Hero.java
@@ -0,0 +1,16 @@
+package lesson09;
+
+public class Hero {
+//	「戦う」と「逃げる」しかできないHeroクラス
+	String name = "ミナト";
+	int hp = 100;
+//	戦う
+	public void attack() {
+		System.out.println(this.name + "の攻撃！");
+		System.out.println("5ポイントのダメージを与えた！");
+	}
+//	逃げる
+	public void run() {
+		System.out.println(this.name + "は逃げ出した！");
+	}
+}
