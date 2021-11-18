043d4c5b4ae0f91ab0da4b21b509fb0ca22b5954
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-11-18 18:14:45 +0900

    [Finish]復習Java入門-7

--- Java学習ファイル/src/practice10/PoisonMatango.java
+++ Java学習ファイル/src/practice10/PoisonMatango.java
@@ -1,19 +1,19 @@
 package practice10;
 
-public class PoisonMatango extends Matango {
-	int poisonCount = 5;
-	
-	public PoisonMatango(char suffix) {
-		super(suffix);
-	}
-	
-	public void attck(Hero h) {
-		super.attack(h);
-		if (this.poisonCount > 0) {
-			int dmg = h.hp / 5;
-			h.hp -= dmg;
-			System.out.println(dmg + "ポイントのダメージ");
-			this.poisonCount--;
-		}
-	}
-}
+//public class PoisonMatango extends Matango {
+//	int poisonCount = 5;
+//	
+//	public PoisonMatango(char suffix) {
+//		super(suffix);
+//	}
+//	
+////	public void attck(Hero h) {
+////		super.attack(h);
+////		if (this.poisonCount > 0) {
+////			int dmg = h.hp / 5;
+////			h.hp -= dmg;
+////			System.out.println(dmg + "ポイントのダメージ");
+////			this.poisonCount--;
+////		}
+////	}
+//}
