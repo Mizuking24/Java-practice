96860b111e6f8a232626e87d3c089df9e7a7a805
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-11-20 20:24:42 +0900

    [Finish]復習Java入門-10

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ae/b0290761f449001c17e1f64f0ded277a
@@ -0,0 +1,16 @@
+package practice10;
+
+public class Matango {
+	int hp = 50;
+	char suffix;
+
+	public Matango(char suffix) {
+		this.suffix = suffix;
+	}
+	
+	public void attck(Hero h) {
+		System.out.println("キノコ" + this.suffix + "攻撃");
+		System.out.println("10のダメージ");
+		h.hp -= 10;
+	}
+}
--- Java学習ファイル/src/practice10/PoisonMatango.java
+++ Java学習ファイル/src/practice10/PoisonMatango.java
@@ -7,13 +7,13 @@ package practice10;
 //		super(suffix);
 //	}
 //	
-////	public void attck(Hero h) {
-////		super.attack(h);
-////		if (this.poisonCount > 0) {
-////			int dmg = h.hp / 5;
-////			h.hp -= dmg;
-////			System.out.println(dmg + "ポイントのダメージ");
-////			this.poisonCount--;
-////		}
-////	}
+//	public void attck() {
+//		super.attack(h);
+//		if (this.poisonCount > 0) {
+//			int dmg = h.hp / 5;
+//			h.hp -= dmg;
+//			System.out.println(dmg + "ポイントのダメージ");
+//			this.poisonCount--;
+//		}
+//	}
 //}
